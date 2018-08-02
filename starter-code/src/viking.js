c9d3cb1aed162e5c1a9f53a701b98c4f08a265c1
Jason Simms <jsimms0110@gmail.com>
2018-08-02 23:20:24 +0200

    completed until Saxon needs to inherit methods.

--- starter-code/src/viking.js
+++ starter-code/src/viking.js
@@ -1,11 +1,55 @@
 // Soldier
-function Soldier() {}
+function Soldier(health, strength) {
+  this.health = health;
+  this.strength = strength;
+  this.attack = function () {
+      return this.strength;
+  };
+  this.receiveDamage = function(damage) {
+      this.health -= damage;
+  };
+}
+
+
+//var vlad = new Soldier(1,2)
+//console.log(vlad);
+
 
 // Viking
-function Viking() {}
+function Viking(name,health,strength) {
+  Soldier.call(this,health,strength);
+  this.name = name;
+  this.receiveDamage = function(damage){
+    this.health -= damage;
+    if (this.health > 0) {
+      var result = name + ' has received '+damage+' points of damage';
+    } else {
+     var result = name + ' has died in act of combat'}
+    return result;
+  }
+  this.battleCry = function(){
+    return 'Odin Owns You All!';
+  }
+  this.attack = function () {
+    return this.strength;
+};
+}
 
 // Saxon
-function Saxon() {}
+function Saxon(health,strength) {
+  Soldier.call(this,health,strength);
+  this.receiveDamage = function(damage){
+    this.health -= damage;
+    if (this.health > 0) {
+      var result = 'A Saxon has received '+damage+' points of damage';
+    } else {
+     var result = 'A Saxon has died in combat'}
+    return result;
+  }
+  this.attack = function () {
+    return this.strength;
+};
+}
 
 // War
 function War() {}
